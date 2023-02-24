63a7765f47f26c7ade5c37882c71ddc3f35a9f6c
HasaNoureddine <hasannoureddine351@gmail.com>
2023-02-24 23:45:19 +0200

    added win/lose status

--- Dice.js
+++ Dice.js
@@ -1,8 +1,23 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   let dice = document.getElementsByClassName("dice");
+  let Dice = document.getElementsByClassName("Dice");
+  console.log(dice);
   for (let i = 0; i < dice.length; i++) {
     dice[i].style.display = "none";
   }
-  dice[Math.floor(Math.random() * dice.length)].style.display = "flex";
-  dice[Math.floor(Math.random() * dice.length)].style.display = "flex";
+  for (let i = 0; i < Dice.length; i++) {
+    Dice[i].style.display = "none";
+  }
+  roll1 = Math.floor(Math.random() * dice.length);
+  roll2 = Math.floor(Math.random() * Dice.length);
+  dice[roll1].style.display = "flex";
+  Dice[roll2].style.display = "flex";
+
+  if (roll1 > roll2) {
+    document.getElementById("status").innerHTML = `PLAYER 1 WINS!`;
+  } else if (roll1 < roll2) {
+    document.getElementById("status").innerHTML = `PLAYER 2 WINS!`;
+  } else {
+    document.getElementById("status").innerHTML = `IT IS A DRAW!`;
+  }
 });
